bd1b7b370669219f9d6d2196294729693714603d
maurigzz <45076297+maurigzz@users.noreply.github.com>
2019-06-24 01:35:20 -0400

    Se agrega validación sobre cantidad de argumentos

--- main.cpp
+++ main.cpp
@@ -13,50 +13,53 @@ using namespace xlnt;
 
 int main(int argc, char *argv[])
 {
-        //Obtiene la información de los docente, almacenando id, nombres, apellidos, pesodisponibilidad, holgura y sus cursos en la clase DOCENTE
-        vector<Docente> vectorDocente = obtenerVectorInfoDocentes(argv);
-        //Obtiene la información de las salas, almacenando el nombre de las salas en la clase SALA
-        vector<Sala> vectorSala = obtenerVectorInfoSalas(argv);
-        // //Obtiene la información de los cursos, almacenando codigo, nombre y bloques en la clase CURSO
-        // vector<Curso> vectorCurso = obtenerVectorInfoCursos(argv);
+	if(argc-1 == 3){
 
-        ordenarPorHolguraVectorDocente(vectorDocente);
+		cout << "Cantidad de Argumentos Valida." << endl;
+                //Obtiene la información de los docente, almacenando id, nombres, apellidos, pesodisponibilidad, holgura y sus cursos en la clase DOCENTE
+                vector<Docente> vectorDocente = obtenerVectorInfoDocentes(argv);
+                //Obtiene la información de las salas, almacenando el nombre de las salas en la clase SALA
+                vector<Sala> vectorSala = obtenerVectorInfoSalas(argv);
+                // //Obtiene la información de los cursos, almacenando codigo, nombre y bloques en la clase CURSO
+                // vector<Curso> vectorCurso = obtenerVectorInfoCursos(argv);
 
-        vector<vector<vector<string> > > superCubo = crearSuperCubo(vectorSala);
-        int ramos = 0;
-        for(int docente = 0; docente < vectorDocente.size(); docente++) { //Se recorren todos los profesores
-                Docente profesor = vectorDocente.at(docente);
-                for(int curso = 0; curso < profesor.getAsignaturas().size(); curso++) { //Se recorren todos los cursos que tiene cada profesor
-                        int bloques =profesor.getAsignaturas().at(curso).getBloques();
-                        string idRamo = profesor.getAsignaturas().at(curso).getCodigo();
-                        int ramoColocado = 0;
-                        bool ramoInformatica = esRamoInformatica(idRamo); //Se verifica si el ramo obtenido es de informatica
+                ordenarPorHolguraVectorDocente(vectorDocente);
 
-                        while(bloques > 0) { //Mientras el ramo aun tenga bloques sin asignar
-                                bool asignar=false;
-                                int salaAleatoria = rand()%vectorSala.size();
-                                bool esLaboratorio = esLab(vectorSala.at(salaAleatoria).getNombre()); //Se verifica si la sala es de LAB
+                vector<vector<vector<string> > > superCubo = crearSuperCubo(vectorSala);
+                int ramos = 0;
+                for(int docente = 0; docente < vectorDocente.size(); docente++) { //Se recorren todos los profesores
+                        Docente profesor = vectorDocente.at(docente);
+                        for(int curso = 0; curso < profesor.getAsignaturas().size(); curso++) { //Se recorren todos los cursos que tiene cada profesor
+                                int bloques =profesor.getAsignaturas().at(curso).getBloques();
+                                string idRamo = profesor.getAsignaturas().at(curso).getCodigo();
+                                int ramoColocado = 0;
+                                bool ramoInformatica = esRamoInformatica(idRamo); //Se verifica si el ramo obtenido es de informatica
 
-                                if(!ramoInformatica && !esLaboratorio) { //Si el ramo no de informatica ni la sala es de laboratorio
-                                        asignar=asignarAsignatura(superCubo,salaAleatoria,profesor,idRamo);
-                                        if (asignar) bloques--;
-                                }
-                                if(ramoInformatica && esLaboratorio) {
-                                        asignar=asignarAsignatura(superCubo,salaAleatoria,profesor,idRamo);
-                                        if (asignar) bloques--;
+                                while(bloques > 0) { //Mientras el ramo aun tenga bloques sin asignar
+                                        bool asignar=false;
+                                        int salaAleatoria = rand()%vectorSala.size();
+                                        bool esLaboratorio = esLab(vectorSala.at(salaAleatoria).getNombre()); //Se verifica si la sala es de LAB
+
+                                        if(!ramoInformatica && !esLaboratorio) { //Si el ramo no de informatica ni la sala es de laboratorio
+                                                asignar=asignarAsignatura(superCubo,salaAleatoria,profesor,idRamo);
+                                                if (asignar) bloques--;
+                                        }
+                                        if(ramoInformatica && esLaboratorio) {
+                                                asignar=asignarAsignatura(superCubo,salaAleatoria,profesor,idRamo);
+                                                if (asignar) bloques--;
+                                        }
+
                                 }
 
                         }
-
                 }
-        }
-        // cout << "Ramos Totales: " << ramos << endl;
-        escribirResultadosEnXlsxFinal(vectorSala, superCubo);
+                // cout << "Ramos Totales: " << ramos << endl;
+                escribirResultadosEnXlsxFinal(vectorSala, superCubo);
+	}
 
-
-
-
-
+	else{
+		cout << "Cantidad de Argumentos Invalida." << endl;
+	}
 
         return 0;
 }
